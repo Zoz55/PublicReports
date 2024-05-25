435b3f2813c385f986a20f9af04666070a8a8e9a
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-05-25 19:12:47 +0300

    Fix Bun

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,8 +2,7 @@ import { sql } from "@vercel/postgres";
  import "dotenv/config";
  
 export const load = async (params) => {
-    console.log(params)
-    const { rows } = await sql`SELECT * from CAR`;
+     const { rows } = await sql`SELECT * from CAR`;
     return {
         car: rows
     }
